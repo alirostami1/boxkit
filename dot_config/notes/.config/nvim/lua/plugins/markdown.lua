f5483174a32bee15f43d20233e60b5bf146dc78d
Ali Rostami <ali.rostami@live.com>
2026-02-27 17:33:50 +0000

    feat: new markdown preview in notes

--- dot_config/notes/.config/nvim/lua/plugins/markdown.lua
+++ dot_config/notes/.config/nvim/lua/plugins/markdown.lua
@@ -1,18 +1,18 @@
 return {
   {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install && git restore .",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
+    "selimacerbas/markdown-preview.nvim",
+    dependencies = { "selimacerbas/live-server.nvim" },
+    config = function()
+      require("markdown_preview").setup({
+        port = 8421,
+        open_browser = false,
+        debounce_ms = 300,
+      })
     end,
-    ft = { "markdown" },
   },
   {
     "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" },
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},

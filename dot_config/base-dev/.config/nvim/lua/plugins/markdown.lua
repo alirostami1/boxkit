11ccc2f214470cf17143bb49bc025d9dcf5edd30
Ali Rostami <ali.rostami@live.com>
2026-02-27 16:09:43 +0000

    feat: changed markdown preview to something maintained

--- dot_config/base-dev/.config/nvim/lua/plugins/markdown.lua
+++ dot_config/base-dev/.config/nvim/lua/plugins/markdown.lua
@@ -1,11 +1,13 @@
 return {
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		build = "cd app && npm install && git restore .",
-		init = function()
-			vim.g.mkdp_filetypes = { "markdown" }
-		end,
-		ft = { "markdown" },
-	},
+  {
+    "selimacerbas/markdown-preview.nvim",
+    dependencies = { "selimacerbas/live-server.nvim" },
+    config = function()
+      require("markdown_preview").setup({
+        port = 8421,
+        open_browser = false,
+        debounce_ms = 300,
+      })
+    end,
+  },
 }
